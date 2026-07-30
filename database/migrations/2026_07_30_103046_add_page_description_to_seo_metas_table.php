a3fe43a189ef401391348078e0bfa77be00b21a8
devsujoy18 <43414517+devsujoy18@users.noreply.github.com>
2026-07-30 10:40:36 +0530

    page_description added to the seo_metas table

---
+++ database/migrations/2026_07_30_103046_add_page_description_to_seo_metas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('seo_metas', function (Blueprint $table) {
+            $table->text('page_description')
+                ->nullable()
+                ->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('seo_metas', function (Blueprint $table) {
+            $table->dropColumn('page_description');
+        });
+    }
+};
